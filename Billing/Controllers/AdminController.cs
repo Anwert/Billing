581a908dd5268d8751c2eef592def412749f55a7
User <user@MacBook-Pro-User-2.local>
2019-03-13 22:27:19 +0500

    added css js template

--- Billing/Controllers/AdminController.cs
+++ Billing/Controllers/AdminController.cs
@@ -11,12 +11,16 @@ namespace Billing.Controllers
 		public AdminController(IClientService client_service)
 		{
 			_clientService = client_service;
+			
+			
 		}
-
+		
 		public async Task<IActionResult> Index()
 		{
 			var clients = await _clientService.Get();
 			return View(clients);
 		}
+		
+//		public async Task<IActionResult> Create()
 	}
 }
